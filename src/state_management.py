2d326fefaf1752f1f205a3e0325148ab9f04130f
Nida Anis <nida.anis@hey.com>
2025-05-03 07:37:01 +0900

    Created state_management.py

---
+++ src/state_management.py
@@ -0,0 +1,41 @@
+"""
+state_management.py
+----------------
+Author: Nida Anis
+Date: 10/02/2025
+----------------
+Description:
+-> Handles event storage and removal
+"""
+
+import streamlit as st
+from datetime import datetime
+import os
+
+def initialise_session_state():
+    """Make sure that session state variables exist."""
+    if "events" not in st.session_state:
+        st.session_state["events"] = []
+
+    if "error_messages" not in st.session_state:
+        st.session_state["error_messages"] = []
+    
+    if "team_logo" not in st.session_state:
+        st.session_state["team logo"] = None
+    
+def save_uploaded_file(uploaded_file):
+    """Saves the uploaded file to a designated folder, updates session state."""
+    os.makedirs("assets", exist_ok=True)
+    file_path = os.path.join("assets", "team_logo.png")
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.session_state["team_logo"] = file_path
+
+def remove_past_events():
+    """Removes past events from session state."""
+    if "events" in st.session_state:
+        st.session_state["events"] = [
+            event for event in st.session_state["events"] if event["end"] > datetime.now()
+        ]
